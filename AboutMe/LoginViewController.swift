6ad75907d41221235c48b18a43b25ab46678ec6a
Chron1um635 <chron1um522@gmail.com>
2024-09-01 19:08:47 +0300

    Setup logic

--- AboutMe/LoginViewController.swift
+++ AboutMe/LoginViewController.swift
@@ -9,11 +9,81 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    let correctLogin = "user"
+    let correctPassword = "111"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginTextField.layer.cornerRadius = 5
+        passwordTextField.layer.cornerRadius = 5
+        loginButton.layer.cornerRadius = 5
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.username = loginTextField.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
+        guard loginTextField.text == correctLogin,
+              passwordTextField.text == correctPassword else {
+            showAlert(
+                with: "Invalid login or password",
+                andMessage: "Please, enter correct login and password"
+            )
+            return false
+        }
+        
+        return true
+    }
+    
+    @IBAction func askLoginButtonAction(_ sender: Any) {
+        showAlert(with: "Oops", andMessage: "Correct login is: \(correctLogin)")
+    }
+    
+    @IBAction func askPasswordButtonAction(_ sender: Any) {
+        showAlert(with: "Oops", andMessage: "Correct password is: \(correctPassword)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private func showAlert(with title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: {
+            _ in
+            self.loginTextField.text = ""
+            self.passwordTextField.text = ""
+        })
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    
+    
+    
+    
 }
 
--- AboutMe/WelcomeViewController.swift
+++ AboutMe/WelcomeViewController.swift
@@ -7,23 +7,38 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var logOutButton: UIButton!
+    
+    var username: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        welcomeLabel.text = "Welcome, \(username ?? "")!"
+        welcomeLabel.textColor = .white
+        
+        logOutButton.layer.cornerRadius = 5
+        logOutButton.backgroundColor = .clear
+        
+        view.applyGradient()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UIView {
+    func applyGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor(
+            red: 0.69,
+            green: 0.31,
+            blue: 0.78,
+            alpha: 1
+        ).cgColor, UIColor.blue.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
     }
-    */
-
 }
